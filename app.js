352711ed179eeef70db07d45531634f92665347c
Morgan Harmon <morganharmon@gmail.com>
2022-03-22 19:23:42 -0700

    adding email input

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-// import functions
+// console.log('hello from app.js');
+// let myName = 'Morgan';
+// console.log(`Hello ${myName}`);
+const button = document.getElementById('button');
 
-// grab DOM elements
+button.addEventListener('click', ()=>{
+// 1. store the element with id email-inputs
+// as a variable 'inputs'
+const inputs = document.getElementById('email-inputs');
+// 2. add the class "hide" to 'inputs'
+inputs.classList.toggle('hide');
+// 3. store the element with the id thank-you
+// as a variable thankYou
+const thankYou = document.getElementById('thank-you');
+// 4. remove the class "hide" from thankYou
+thankYou.classList.toggle('hide');
+// 5. get email input
+var x = document.getElementById('button').value;
+// 6. print email input to console
+x.value
+});
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
